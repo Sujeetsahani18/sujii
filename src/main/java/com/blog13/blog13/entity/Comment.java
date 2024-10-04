f492807fd45da684068d03f74c4ccbae0dec284c
sujeet <sujeetsahani@gmail.com>
2024-10-04 05:24:06 -0700

    test1

---
+++ src/main/java/com/blog13/blog13/entity/Comment.java
@@ -0,0 +1,54 @@
+package com.blog13.blog13.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "measge", nullable = false)
+    private String measge;
+
+    public String getMeasge() {
+        return measge;
+    }
+
+    public void setMeasge(String measge) {
+        this.measge = measge;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
